26da0687357f0d4055b6089c7495ebbd8460224b
surister <surister98@gmail.com>
2019-12-16 12:16:05 +0100

    question counter script improved

--- scripts/count_questions.sh
+++ scripts/count_questions.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo $((7 + $(cat README.md | grep \<\/summary\> | wc -l)))
+# We dont care about non alphanumerics filenames so we just ls | grep to shortern the script.
+
+echo $(( $(ls ./scenarios/ | grep ".*md" -c) + $(grep \</summary\> -c README.md) ))
