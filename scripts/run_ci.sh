cc95d4631eb0ba640a6bbf335f9f1d0b5723b29c
surister <surister98@gmail.com>
2020-01-09 12:07:41 +0100

    Rename syntax_checker into syntax_lint

--- scripts/run_ci.sh
+++ scripts/run_ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # These are the same steps we are running in Travis CI
 
-python tests/syntax_checker.py
+python tests/syntax_lint.py
 flake8 --max-line-length=100 . && echo "PEP8 Passed"
