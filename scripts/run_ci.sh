76ddfd3c9874548f51e6886c3c1702ea23181810
Lewis Brogan <ilewisbrogan@gmail.com>
2022-06-26 18:49:43 +0100

    removed venv from flask8 local testing

--- scripts/run_ci.sh
+++ scripts/run_ci.sh
@@ -2,4 +2,4 @@
 # These are the same steps we are running in Travis CI
 
 python tests/syntax_lint.py
-flake8 --exclude venv --max-line-length=100 . && echo "PEP8 Passed"
+flake8 --max-line-length=100 . && echo "PEP8 Passed"
