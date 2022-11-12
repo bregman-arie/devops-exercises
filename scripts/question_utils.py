2cb540a547a8823f6325f8ac03d76ee585f2acc7
Melany Estevez Camata <63152500+Mademoisellefou@users.noreply.github.com>
2022-11-12 12:17:45 -0400

    Update question_utils.py
    running flake8

--- scripts/question_utils.py
+++ scripts/question_utils.py
@@ -5,79 +5,85 @@ Question utils functions
 import pathlib
 from random import choice
 from typing import List
+import re
 
-p = pathlib.Path(__file__).parent.parent.joinpath('README.md')
+p = pathlib.Path(__file__).parent.parent.joinpath("README.md")
 
 
 def get_file_list():
-    with open(p, 'rb') as f:
-        file_list = [line.rstrip() for line in f.readlines()]
+    file_list = ""
+    with open(p, "rb") as f:
+        for line in f.readlines():
+            file_list += line.rstrip().decode()
     return file_list
 
+
 def get_question_list(file_list: List[str]) -> list:
-    file_list =re.findall('<details>(.*?)</details>',file_list)
+    file_list = re.findall("<details>(.*?)</details>", file_list)
     questions_list = []
     for i in file_list:
-        q=re.findall(r'<summary>(.*?)</summary>',i)[0]
+        q = re.findall(r"<summary>(.*?)</summary>", i)[0]
         questions_list.append(q)
     return questions_list
 
 
 def get_answered_questions(question_list: List[str]) -> list:
-    t=[]
-    question_list =re.findall('<details>(.*?)</details>',question_list)
+    t = []
+    question_list = re.findall("<details>(.*?)</details>", question_list)
     for i in question_list:
-        q=re.findall(r'<summary>(.*?)</summary>',i)
-        if q and q[0]=='':
+        q = re.findall(r"<summary>(.*?)</summary>", i)
+        if q and q[0] == "":
             continue
-        a=re.findall(r'<b>(.*?)</b>',i)
-        if a and a[0]=='':
+        a = re.findall(r"<b>(.*?)</b>", i)
+        if a and a[0] == "":
             continue
         else:
             t.append(q[0])
     return t
 
+
 def get_answers_count() -> List:
     """
-    
-    Return the number of questions answered and the number of all questions ,PASS if it is complete.
-
+    Return [answer_questions,all_questions] ,PASS complete. FAIL incomplete.
     >>> get_answers_count()
-    [1, 1]
+    [463, 463]
     """
-    ans_questions=get_answered_questions(get_file_list())
-    len_ans_questions=len(ans_questions)
-    all_questions=get_question_list(get_file_list())
-    len_all_questions=len(all_questions)
-    return [len_ans_questions,len_all_questions]
+    ans_questions = get_answered_questions(get_file_list())
+    len_ans_questions = len(ans_questions)
+    all_questions = get_question_list(get_file_list())
+    len_all_questions = len(all_questions)
+    return [len_ans_questions, len_all_questions]
+
 
 def get_challenges_count() -> int:
-    challenges_path = pathlib.Path(__file__).parent.parent.joinpath('exercises').glob('*.md')
+    challenges_path = (
+        pathlib.Path(__file__).parent.parent.joinpath("exercises").glob("*.md")
+    )
     return len(list(challenges_path))
 
 
 # WIP WAITING FEEDBACK
-def get_random_question(question_list: List[List[bytes]], with_answer=False):
+def get_random_question(question_list: List[str], with_answer=False):
     if with_answer:
         return choice(get_answered_questions(question_list))
-    return choice(question_list)
+    return choice(get_question_list(question_list))
 
 
 """Use this question_list. Unless you have already opened/worked/need the file, then don't or
 you will end up doing the same thing twice.
-
 eg:
-
 #my_dir/main.py
-
 from scripts import question_utils
-
 print(question_utils.get_answered_questions(question_utils.question_list)
-
 >> 123
-
+ # noqa: E501
 """
 
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
+    # print(get_question_list(get_file_list()))
+    # print(get_answered_questions(get_file_list()))
+    # print(get_random_question(get_file_list(),True))
+    # print(get_random_question(get_file_list(),False))
